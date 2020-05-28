0062623bd789e6cbaa08eb0c868fff4f327ab0d6
Anas Qazi <ianasqazi@gmail.com>
2020-05-28 11:52:17 -0400

    pwa added successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,6 @@ import { environment } from '../environments/environment';
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
     }),
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent],
